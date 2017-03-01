ce1b7db1e203ef59d2e879adb49faea42bb900f8
Console Admin <faztp12@gmail.com>
2017-03-01 09:17:17 +0300

    Territory Payments Fix

--- consoleerp_erpnext_client/app_data/territory_branch/custom_scripts/Payment Entry.js
+++ consoleerp_erpnext_client/app_data/territory_branch/custom_scripts/Payment Entry.js
@@ -1,1 +1,21 @@
 cur_frm.add_fetch("consoleerp_territory", "consoleerp_abbr", "consoleerp_territory_abbr");
+
+frappe.ui.form.on("Payment Entry",{
+	onload : function(frm) {
+				
+		frappe.after_ajax(function() {
+		
+			if (frm.doc.references && frm.doc.references[0])
+			{
+				var ref = frm.doc.references[0];
+				frappe.db.get_value(ref.reference_doctype, ref.reference_name, ["consoleerp_territory", "consoleerp_territory_abbr"], function(v) {					
+					if (!v.consoleerp_territory)
+						return;
+					
+					frappe.model.set_value("Payment Entry", frm.doc.name, "consoleerp_territory", v.consoleerp_territory);
+					frappe.model.set_value("Payment Entry", frm.doc.name, "consoleerp_territory_abbr", v.consoleerp_territory_abbr);
+				});
+			}
+		});
+	}
+});
--- consoleerp_erpnext_client/app_data/territory_branch/custom_scripts/Payment Request.js
+++ consoleerp_erpnext_client/app_data/territory_branch/custom_scripts/Payment Request.js
@@ -1,1 +1,20 @@
 cur_frm.add_fetch("consoleerp_territory", "consoleerp_abbr", "consoleerp_territory_abbr");
+
+frappe.ui.form.on("Payment Request",{
+	onload : function(frm) {
+				
+		frappe.after_ajax(function() {
+		
+			if (frm.doc.reference_name)
+			{				
+				frappe.db.get_value(frm.doc.reference_doctype, frm.doc.reference_name, ["consoleerp_territory", "consoleerp_territory_abbr"], function(v) {					
+					if (!v.consoleerp_territory)
+						return;
+					
+					frappe.model.set_value("Payment Request", frm.doc.name, "consoleerp_territory", v.consoleerp_territory);
+					frappe.model.set_value("Payment Request", frm.doc.name, "consoleerp_territory_abbr", v.consoleerp_territory_abbr);
+				});
+			}
+		});
+	}
+});
