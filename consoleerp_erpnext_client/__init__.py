dc78bc2d73d7e4c5904291c14d17561ad0d5cfb3
Console Admin <faztp12@gmail.com>
2017-05-25 08:51:29 +0300

    Queue submission manually

--- consoleerp_erpnext_client/__init__.py
+++ consoleerp_erpnext_client/__init__.py
@@ -8,3 +8,10 @@ __version__ = '1.0.0'
 def backup_to_google_drive():
 	from consoleerp_erpnext_client.utils.google_drive import test
 	test()
+	
+def queue_doc_submit(doctype, name):
+	doc = frappe.get_doc(doctype, name)
+	if doc.docstatus == 1:
+		print("This document is already submitted")
+	else:
+		doc.queue_action('submit')
