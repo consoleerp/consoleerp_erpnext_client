a9bb9e93956d4fb57f92f7b3a484429fed4c342d
Console Admin <faztp12@gmail.com>
2017-07-24 08:35:40 +0300

    Notifications

---
+++ consoleerp_erpnext_client/public/js/notification.js
@@ -0,0 +1,41 @@
+$(document).on('startup', function(){
+	// on startup
+	if (!frappe.boot.consoleerp)
+		return;
+	
+	if (frappe.boot.consoleerp.expiring_documents) {
+		var rows = frappe.boot.consoleerp.expiring_documents.reduce(function(str, obj){
+			return str 
+			+ "<tr>"
+				+ "<td>"+ obj.doc_no +"</td>"
+				+ "<td>"+ obj.type +"</td>"
+				+ "<td>"+ obj.expiry_date +"</td>"
+				+ "<td>"
+					+ "<a data-parent='"+obj.parent+"' data-parenttype='"+obj.parenttype+"'>"
+						+ obj.parent 
+					+"</a>"
+				+ "</td>"
+			+ "</tr>";
+		}, "");
+		var $wrapper = frappe.msgprint("<h3>Expiring Documents</h3>"
+							+ "<br>The following documents will expire soon."
+							+ "<table class='table table-striped table-hover'>"
+								+ "<thead>"
+									+ "<tr>"
+										+ "<th>" + __("Doc No") + "</th>"
+										+ "<th>" + __("Type") + "</th>"
+										+ "<th>" + __("Expiry Date") + "</th>"
+										+ "<th>" + __("Parent") + "</th>"
+									+ "</tr>"
+								+ "</thead>"
+								+ "<tbody>"
+								+ rows
+								+ "</tbody>"
+							+ "</table>"
+							+ "<hr>"
+							, "Console ERP Notifications").$wrapper;		
+		$wrapper.find("a").on("click", function(){
+			frappe.set_route("Form", $(this).data("parenttype"), $(this).data("parent"));
+		})
+	}
+});
